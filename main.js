f6c78089c6174665ebf64e227311bc765641ce61
Burur1ch <Irresistible0@mail.ru>
2022-08-01 15:01:30 +0300

    To Do Mayker

---
+++ main.js
@@ -0,0 +1,39 @@
+const toDoItems = document.getElementsByClassName("to-do-items")[0];
+const input = document.getElementById("input")
+const trashIcon = document.getElementById("trash")
+const checkIcon = document.getElementById("check")
+
+input.addEventListener('keydown', function(event){
+    if(event.key === "Enter")
+        addItem();
+})
+
+function addItem(){
+    var divParent = document.createElement("div");
+    var divChild = document.createElement("div");
+    var checkIcon = document.createElement("i");
+    var trashIcon = document.createElement("i");
+
+    divParent.className="item";
+    divParent.innerHTML = '<div>'+input.value +'</div>'; 
+
+    checkIcon.className="fa-solid fa-circle-check";
+    checkIcon.style.color = "lightgray";
+    checkIcon.addEventListener("click",function(){
+        checkIcon.style.color = 'limegreen';
+    })
+
+    divChild.appendChild(checkIcon);
+
+    trashIcon.className="fa-solid fa-trash-can";
+    trashIcon.style.color = "darkgray";
+    trashIcon.addEventListener("click",function(){
+        divParent.remove();
+    })
+
+    divChild.appendChild(trashIcon);
+    divParent.appendChild(divChild);
+    toDoItems.appendChild(divParent);
+    input.value='';
+ 
+}
